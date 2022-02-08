fad6f86bc032e778b4d6ecd28820a40040fad4dc
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-02-08 11:51:24 +0300

    initialize

---
+++ src/test/java/com/example/coursework2question/CourseWork2QuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2QuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
